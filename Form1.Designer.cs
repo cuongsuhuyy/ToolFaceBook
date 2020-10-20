e5f413712d2675adc47826902a82bd1a5e407375
cuongsuhuyy <cuongsuhuy@gmail.com>
2020-10-20 14:25:34 +0700

    Init Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ToolFaceBook02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_Password = new System.Windows.Forms.Label();
+            this.lb_UserName = new System.Windows.Forms.Label();
+            this.tb_UserName = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.btn_SignIn = new System.Windows.Forms.Button();
+            this.lb_CountLoop = new System.Windows.Forms.Label();
+            this.lb_CheckStatusStory = new System.Windows.Forms.Label();
+            this.btn_StopStory = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb_Password
+            // 
+            this.lb_Password.AutoSize = true;
+            this.lb_Password.Location = new System.Drawing.Point(21, 54);
+            this.lb_Password.Name = "lb_Password";
+            this.lb_Password.Size = new System.Drawing.Size(73, 17);
+            this.lb_Password.TabIndex = 0;
+            this.lb_Password.Text = "Password:";
+            // 
+            // lb_UserName
+            // 
+            this.lb_UserName.AutoSize = true;
+            this.lb_UserName.Location = new System.Drawing.Point(21, 18);
+            this.lb_UserName.Name = "lb_UserName";
+            this.lb_UserName.Size = new System.Drawing.Size(79, 17);
+            this.lb_UserName.TabIndex = 1;
+            this.lb_UserName.Text = "UserName:";
+            // 
+            // tb_UserName
+            // 
+            this.tb_UserName.Location = new System.Drawing.Point(107, 12);
+            this.tb_UserName.Name = "tb_UserName";
+            this.tb_UserName.Size = new System.Drawing.Size(100, 22);
+            this.tb_UserName.TabIndex = 2;
+            this.tb_UserName.Text = "cuongsu0072013@yahoo.com";
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Location = new System.Drawing.Point(107, 48);
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.PasswordChar = '*';
+            this.tb_Password.Size = new System.Drawing.Size(100, 22);
+            this.tb_Password.TabIndex = 3;
+            this.tb_Password.Text = "khongbiet";
+            // 
+            // btn_SignIn
+            // 
+            this.btn_SignIn.Location = new System.Drawing.Point(24, 89);
+            this.btn_SignIn.Name = "btn_SignIn";
+            this.btn_SignIn.Size = new System.Drawing.Size(136, 47);
+            this.btn_SignIn.TabIndex = 4;
+            this.btn_SignIn.Text = "Đăng Nhập";
+            this.btn_SignIn.UseVisualStyleBackColor = true;
+            this.btn_SignIn.Click += new System.EventHandler(this.btn_SignIn_Click);
+            // 
+            // lb_CountLoop
+            // 
+            this.lb_CountLoop.AutoSize = true;
+            this.lb_CountLoop.Location = new System.Drawing.Point(298, 54);
+            this.lb_CountLoop.Name = "lb_CountLoop";
+            this.lb_CountLoop.Size = new System.Drawing.Size(46, 17);
+            this.lb_CountLoop.TabIndex = 5;
+            this.lb_CountLoop.Text = "label1";
+            // 
+            // lb_CheckStatusStory
+            // 
+            this.lb_CheckStatusStory.AutoSize = true;
+            this.lb_CheckStatusStory.Location = new System.Drawing.Point(381, 18);
+            this.lb_CheckStatusStory.Name = "lb_CheckStatusStory";
+            this.lb_CheckStatusStory.Size = new System.Drawing.Size(110, 17);
+            this.lb_CheckStatusStory.TabIndex = 6;
+            this.lb_CheckStatusStory.Text = "Running Story...";
+            // 
+            // btn_StopStory
+            // 
+            this.btn_StopStory.Location = new System.Drawing.Point(251, 113);
+            this.btn_StopStory.Name = "btn_StopStory";
+            this.btn_StopStory.Size = new System.Drawing.Size(136, 44);
+            this.btn_StopStory.TabIndex = 7;
+            this.btn_StopStory.Text = "Stop Run Story";
+            this.btn_StopStory.UseVisualStyleBackColor = true;
+            this.btn_StopStory.Click += new System.EventHandler(this.btn_StopStory_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(443, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 173);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_StopStory);
+            this.Controls.Add(this.lb_CheckStatusStory);
+            this.Controls.Add(this.lb_CountLoop);
+            this.Controls.Add(this.btn_SignIn);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_UserName);
+            this.Controls.Add(this.lb_UserName);
+            this.Controls.Add(this.lb_Password);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_Password;
+        private System.Windows.Forms.Label lb_UserName;
+        private System.Windows.Forms.TextBox tb_UserName;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.Button btn_SignIn;
+        private System.Windows.Forms.Label lb_CountLoop;
+        private System.Windows.Forms.Label lb_CheckStatusStory;
+        private System.Windows.Forms.Button btn_StopStory;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ToolFaceBook02
+{
+    public partial class Form1 : Form
+    {
+        private bool flag_wait_forresponse = true;
+        private bool flag_Check_thread_is_running_Story = false;
+
+        ThreadStart ts;
+        Thread thrd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ts = new ThreadStart(Run);
+            thrd = new Thread(ts);
+        }
+
+        void Run()
+        {
+            String chromeDriverPath = @"D:\RAR\ChromeDriver_x86\chromedriver_win32";
+            var options = new ChromeOptions();
+            options.AddArgument("no-sandbox");
+
+            options.AddArguments("--disable-notifications");
+
+            var driver = new ChromeDriver(chromeDriverPath, options, TimeSpan.FromDays(20));
+            string url = "https://www.facebook.com/";
+            driver.Url = url;
+            driver.Navigate().GoToUrl(url);
+
+            while (flag_wait_forresponse == true)
+            {
+                try
+                {
+                    IWebElement UserName = driver.FindElement(By.CssSelector("input[type='text']"));
+                    UserName.SendKeys(tb_UserName.Text);
+
+                    IWebElement Password = driver.FindElement(By.CssSelector("input[type='password']"));
+                    Password.SendKeys(tb_Password.Text);
+
+                    IWebElement sign_in = driver.FindElement(By.CssSelector("button[name='login']"));
+                    sign_in.Click();
+
+                    flag_wait_forresponse = false;
+                }
+                catch
+                {
+                }
+            }
+            flag_wait_forresponse = true;
+
+            while (flag_wait_forresponse == true)
+            {
+                try
+                {
+                    IWebElement WatchStory = driver.FindElement(By.CssSelector("div[aria-label='Tin']"));
+                    WatchStory.Click();
+
+                    flag_wait_forresponse = false;
+                }
+                catch
+                {
+                }
+            }
+            flag_wait_forresponse = true;
+
+            while (flag_wait_forresponse == true)
+            {
+                try
+                {
+                    IWebElement PauseStory = driver.FindElement(By.CssSelector("div[aria-label='Phát']"));
+                    PauseStory.Click();
+                    flag_wait_forresponse = false;
+                }
+                catch
+                {
+                    try 
+                    {
+                        IWebElement PauseStory = driver.FindElement(By.CssSelector("div[aria-label='Tạm dừng']"));
+                        PauseStory.Click();
+                        flag_wait_forresponse = false;
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+            flag_wait_forresponse = true;
+
+            try
+            {
+                //for (int y = 0; y < 5; y++)
+                //{
+                    IWebElement Yeu_Thich = driver.FindElement(By.CssSelector("span[aria-label='Yêu thích']"));
+                    Yeu_Thich.Click();
+                //}
+            }
+            catch
+            {
+
+            }
+
+            int i;
+            for (i = 0; i < 100; i++)
+            {
+                while (flag_wait_forresponse == true)
+                {
+                    try
+                    {
+                        IWebElement NextStory = driver.FindElement(By.CssSelector("div[aria-label='Nút Tin tiếp theo']"));
+                        NextStory.Click();
+                        flag_wait_forresponse = false;
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            IWebElement NextStory = driver.FindElement(By.CssSelector("div[aria-label='Nút thẻ tiếp theo']"));
+                            NextStory.Click();
+                            flag_wait_forresponse = false;
+                        }
+                        catch
+                        { }
+                    }
+                }
+                flag_wait_forresponse = true;
+
+                while (flag_wait_forresponse == true)
+                {
+                    try
+                    {
+                        IWebElement PauseStory = driver.FindElement(By.CssSelector("div[aria-label='Phát']"));
+                        PauseStory.Click();
+                        flag_wait_forresponse = false;
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            IWebElement PauseStory = driver.FindElement(By.CssSelector("div[aria-label='Tạm dừng']"));
+                            PauseStory.Click();
+                            flag_wait_forresponse = false;
+                        }
+                        catch { }
+                    }
+                }
+                flag_wait_forresponse = true;
+
+                try
+                {
+                    //for (int y = 0; y < 5; y++)
+                    //{
+                        IWebElement Yeu_Thich = driver.FindElement(By.CssSelector("span[aria-label='Yêu thích']"));
+                        Yeu_Thich.Click();
+                    //}
+                }
+                catch
+                {
+
+                }
+                lb_CountLoop.Text = i.ToString();
+            }
+            flag_Check_thread_is_running_Story = false;
+            lb_CheckStatusStory.Text = flag_Check_thread_is_running_Story.ToString();
+        }
+
+        void Run_run()
+        {
+            ThreadStart ts2 = new ThreadStart(Run);
+            Thread thrd2 = new Thread(ts2);
+            while(flag_Check_thread_is_running_Story == true)
+            {
+                thrd2.Start();
+            }
+            thrd2.Abort();
+        }
+
+        
+
+        private void btn_SignIn_Click(object sender, EventArgs e)
+        {
+            flag_Check_thread_is_running_Story = true;
+            lb_CheckStatusStory.Text = flag_Check_thread_is_running_Story.ToString();
+            thrd.Start();
+        }
+
+        private void btn_StopStory_Click(object sender, EventArgs e)
+        {
+            thrd.Abort();
+            lb_CheckStatusStory.Text = flag_Check_thread_is_running_Story.ToString();
+        }
+    }
+}
